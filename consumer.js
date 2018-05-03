336d6c1a5bcffb1be1e2718a756f1308579ca129
gauravko <gaurav.gprs@gmail.com>
2018-05-03 13:52:55 +0530

    first

---
+++ consumer.js
@@ -0,0 +1,38 @@
+import kafka from "kafka-node"
+ 
+const client = new kafka.Client("localhost:2181");
+ 
+const topics = [
+    {
+        topic: "webevents.dev"
+    }
+];
+const options = {
+    autoCommit: true,
+    fetchMaxWaitMs: 1000,
+    fetchMaxBytes: 1024 * 1024,
+    encoding: "buffer"
+};
+ 
+const consumer = new kafka.HighLevelConsumer(client, topics, options);
+ 
+consumer.on("message", function(message) {
+ 
+    // Read string into a buffer.
+    var buf = new Buffer(message.value, "binary"); 
+    var decodedMessage = JSON.parse(buf.toString());
+ 
+    //Events is a Sequelize Model Object. 
+    console.log('Received Msg .. printing .............');
+    console.log(decodedMessage);
+});
+ 
+consumer.on("error", function(err) {
+    console.log("error", err);
+});
+ 
+process.on("SIGINT", function() {
+    consumer.close(true, function() {
+        process.exit();
+    });
+});
---
+++ server.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const Hapi=require('hapi');
+import KafkaService from './producer';
+// Create a server with a host and port
+const server=Hapi.server({
+    host:'localhost',
+    port:8000
+});
+
+// Add the route
+server.route([
+    {
+        method:'GET',
+        path:'/hello',
+        handler:function(request,h) {
+
+            return'hello world';
+        }
+    },
+    {
+
+        method: 'GET',
+        path: '/sendEvent',
+        handler: function(request, h){
+            const { type, userId, sessionId, partitionId} = request.query;
+            KafkaService.sendRecord({ type, userId, sessionId} );
+            return "added to que";
+        }
+    }
+
+]);
+
+// Start the server
+async function start() {
+
+    try {
+        await server.start();
+    }
+    catch (err) {
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('Server running at:', server.info.uri);
+};
+
+start();
